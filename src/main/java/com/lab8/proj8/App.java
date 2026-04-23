439524691d319961d575b6db5d05816734faa8e9
shashank <shashankhegdecs@gmail.com>
2026-04-23 11:36:53 +0530

    first commit

---
+++ src/main/java/com/lab8/proj8/App.java
@@ -0,0 +1,13 @@
+package com.lab8.proj8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
